7db0b6fb8fe724d6a637d44b157a1fbd235ebfd6
Snooker4real <cindanojonathan@icloud.com>
2021-12-12 21:12:40 +0100

    day 8 added

---
+++ day_8.js
@@ -0,0 +1,13 @@
+console.log("Day 8");
+
+const DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
+
+function getDaysBetweenDates(dateText1, dateText2) {
+    // write your solution here
+    const date1 = new Date(dateText1);
+    const date2 = new Date(dateText2);
+    const diff = Math.abs(date2 - date1);
+    return (diff/DAY_IN_MILLISECONDS);
+}
+
+console.log(`Days difference: ${getDaysBetweenDates('10/15/2020', '12/1/2020')}`)
