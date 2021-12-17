7f0dc8f2a35a882000c3efa423cb4a56a34abbc4
Snooker4real <cindanojonathan@icloud.com>
2021-12-17 22:55:26 +0100

    Adding Day 11

---
+++ day_11.js
@@ -0,0 +1,12 @@
+function fibonacci(n) {
+	// write your solution here
+	if (n === 0) {
+		return 0;
+	} else if (n === 1) {
+		return 1;
+	} else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
+
+console.log(`fibonacci value at position 5: ${fibonacci(10)}`)
