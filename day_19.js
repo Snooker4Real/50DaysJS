e956893239db3b922fb8f68e38f244f16ae1eab9
Snooker4real <cindanojonathan@icloud.com>
2021-12-19 21:11:03 +0100

    Day 19

---
+++ day_19.js
@@ -0,0 +1,17 @@
+console.log("Day 19");
+
+const colorCode = (color) => {
+    // Code here
+    color = color.toLowerCase();
+	switch(color){
+        case 'brown' : return  1;
+        case 'red' : return  2;
+        case 'yellow' : return  4;
+        case 'green' : return  5;
+        case 'blue' : return  6;
+        case 'violet' : return  7;
+        case 'grey' : return  8;
+        case 'white' : return 9;
+        default : return  0;
+    }
+}
